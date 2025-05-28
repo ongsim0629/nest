606b4885863d0e24fe42ed1789b1079884e68c5e
ongsim0629 <s1_1v@naver.com>
2025-05-28 15:46:40 +0900

    🔥 : 사용하지 않는 파일과 코드 제거

--- contract-app/server/contract/contract.module.ts
+++ contract-app/server/contract/contract.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ContractController } from './contract.controller';
 import { ContractService } from './contract.service';
-import { Prisma } from '@/generated/prisma';
 import { PrismaService } from '../prisma.service';
 
 @Module({
